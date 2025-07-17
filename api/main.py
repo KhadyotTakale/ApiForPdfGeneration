49aa10d53be8a4c1055d6a6e294ce5447eefdc09
KhadyotTakale <112562949+KhadyotTakale@users.noreply.github.com>
2025-07-17 21:54:40 +0530

    Update with cors main.py

--- api/main.py
+++ api/main.py
@@ -1,18 +1,29 @@
 import os
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import StreamingResponse, JSONResponse
+import io
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeout
 from urllib.parse import urlparse
-import io
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class PDFRequest(BaseModel):
     url: str
 
 @app.post("/generate-pdf")
 def generate_pdf(payload: PDFRequest):
+    # Extract ID from the URL
     path = urlparse(payload.url).path
     invoice_id = path.rstrip("/").split("/")[-1]
     print("Extracted ID:", invoice_id)
@@ -25,7 +36,8 @@ def generate_pdf(payload: PDFRequest):
             try:
                 page.wait_for_selector("body", timeout=5000)
             except PlaywrightTimeout:
-                print("⚠️ Body not found in time.")
+                print("Body not found in time.")
+
             pdf_bytes = page.pdf(format="A4", print_background=True)
             browser.close()
     except Exception as e:
@@ -36,7 +48,7 @@ def generate_pdf(payload: PDFRequest):
         media_type="application/pdf",
         headers={
             "Content-Disposition": f"attachment; filename={invoice_id}.pdf",
-            "X-INVOICE-ID": invoice_id  
+            "X-INVOICE-ID": invoice_id
         }
     )
 
